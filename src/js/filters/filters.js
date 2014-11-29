ae729487223b9ca4d7459e22124de98cdf5f8095
Marc Whitbread <marcwhitbread@gmail.com>
2014-11-29 18:09:29 -0500

    added filters.js to the repo and grunt watch for directory

---
+++ src/js/filters/filters.js
@@ -0,0 +1,16 @@
+//convert seconds to mm:ss format
+var filterTime = function() {
+	return function(seconds) {
+		
+		var mins = Math.floor(seconds/60);
+		var secs = Math.floor(seconds%60);
+		
+		//add leading zero
+		secs = (secs.toString().length < 2) ? '0' + secs : secs;
+
+		return mins + ':' + secs;
+		
+	}
+}
+
+app.filter('filterTime', filterTime);
